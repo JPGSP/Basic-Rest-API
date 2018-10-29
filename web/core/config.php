1cf54881f69ee9e237817f4f36093831ea9c65af
Joaquin Pino <joaquin.pino@crowd2fund.com>
2018-10-29 21:59:50 +0000

    Configuration connection to the database

---
+++ web/core/config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JPG
+ * Date: 28/10/2018
+ * Time: 19:24
+ */
+
+ define("DB_HOST", "basic-mariadb");
+ define("DB_USER", "root");
+ define("DB_PWD", "password");
+ define("DB_NAME", "basic");
+?>
